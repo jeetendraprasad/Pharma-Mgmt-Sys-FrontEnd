2bd20dec4b87d2fbbac4d5e67444ff23b9ad4b14
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-08-29 00:29:32 +0530

    //cloud

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 
 const constants = {
+    //cloud
     appUrl: "http://localhost:4000" // NOTE: No trailing slash (/)
 	//appUrl: "http://35.200.216.204:4000" // NOTE: No trailing slash (/) And this will change evry time.
 };
