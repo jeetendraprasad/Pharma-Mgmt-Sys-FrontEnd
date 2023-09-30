f3cd5da73f1e5387db170a4e41f40ab985502d8a
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-09-30 12:34:33 +0530

    version 8

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,7 +38,7 @@ export default function Header() {
                           alt="Inventory Management System"
                         />
                         <span className="font-bold text-white italic">
-                          Pharmacy Management v7
+                          Pharmacy Management v8
                         </span>
                       </div>
                     </div>
